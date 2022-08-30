9bdeb30418a3055e651c3d7ad6db2c5bbb6df3d7
LindsayLew <lmschwehr@gmail.com>
2022-08-29 23:32:42 -0700

    email

--- rpg-p3-app/src/pages/CreateAccount.js
+++ rpg-p3-app/src/pages/CreateAccount.js
@@ -3,7 +3,10 @@ export default function createAccount() {
         <section>
             <form>
                 <section className="row mb-2">
-                    
+                    <label for="emailInput" class="col-sm-2 col-form-label">Email</label>
+                    <section className="col-sm-10">
+                        <input type="email" class="form-control"/>
+                    </section>
                 </section>
             </form>
         </section>
