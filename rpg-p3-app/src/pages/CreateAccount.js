5942d078fcf0b39113397bf5e5bea8db092de3ae
LindsayLew <lmschwehr@gmail.com>
2022-08-29 23:27:43 -0700

    initial create account page

---
+++ rpg-p3-app/src/pages/CreateAccount.js
@@ -0,0 +1,11 @@
+export default function createAccount() {
+    return (
+        <section>
+            <form>
+                <section className="row mb-2">
+                    
+                </section>
+            </form>
+        </section>
+    )
+}
