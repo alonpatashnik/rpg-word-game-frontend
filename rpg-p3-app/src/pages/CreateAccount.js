9517d9d26a04c9da74d292066d4e2a1b3526cd63
LindsayLew <lmschwehr@gmail.com>
2022-08-29 23:33:51 -0700

    password

--- rpg-p3-app/src/pages/CreateAccount.js
+++ rpg-p3-app/src/pages/CreateAccount.js
@@ -14,6 +14,12 @@ export default function createAccount() {
                         <input type="text" class="form-control"/>
                     </section>
                 </section>
+                <section className="row mb-2">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <section className="col-sm-10">
+                        <input type="password" class="form-control"/>
+                    </section>
+                </section>
             </form>
         </section>
     )
