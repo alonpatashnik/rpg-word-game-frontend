13b597eb0acf6f01b2a0b3a45fa43b427ae0e87e
LindsayLew <lmschwehr@gmail.com>
2022-08-30 11:35:11 -0700

    starting export mainwindow

--- rpg-p3-app/src/components/mainWindow.js
+++ rpg-p3-app/src/components/mainWindow.js
@@ -2,3 +2,11 @@ import React, { useState } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import createAccount from './pages/CreateAccount';
+
+export default function MainWindow() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        
+    }
+}
