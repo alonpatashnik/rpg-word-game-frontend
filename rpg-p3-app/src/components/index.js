6dc6dfd7e15992157f8d2d60891c6d664e7d063b
LindsayLew <lmschwehr@gmail.com>
2022-08-29 19:59:05 -0700

    testing

--- rpg-p3-app/src/components/index.js
+++ rpg-p3-app/src/components/index.js
@@ -2,3 +2,21 @@ import React, { PureComponent } from 'react';
 import { GameEngine } from 'react-game-engine';
 import { Box } from './renderers';
 import { MoveBox } from './systems';
+
+export default class SimpleGame extends PureComponent {
+    render() {
+        return (
+            <GameEngine
+                style={{ width: 800, height: 600, backgroundColor: "blue" }}
+                systems={[MoveBox]}
+                entities={{
+                    // each entity has a unique id (required)
+                    // and a renderer property (which is optional)
+                    // if no renderer is with the entity, it won't be displayed
+                    box1: { x: 200, y:200, renderer: <Box />}
+                }}>
+    
+                </GameEngine>
+        );
+    }
+}
