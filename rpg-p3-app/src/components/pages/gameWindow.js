c2ff029b8b167386fd5be4b6267a9f322eba4eb2
LindsayLew <lmschwehr@gmail.com>
2022-08-31 11:07:18 -0700

    come back to error later

--- rpg-p3-app/src/components/pages/gameWindow.js
+++ rpg-p3-app/src/components/pages/gameWindow.js
@@ -1,22 +1,39 @@
 // game window will then hold the entirety of the game, so no more pages after this one.
-
+import './style/game'
 import 'url("https://fonts.googleapis.com/css2?family=Silkscreen&display=swap")'
 
-function Game() {
- return (
-    // <!-- think I'm going to use an offcanvas component for the side nav -->
-    <section className="titleCon">
-        <p className="text-start fs-1">
-            Game Title
-        </p>
-        <button class="btn" data-bs-toggle="offcanvas" href="#offCanvasMenu" role="button" aria-controls="offCanvasMenu" >
-            menu
-        </button>
-    </section>
-    // <!-- offcanvas -->
-    <section class="offcanvas offcanvas-end show text-bg-dark" tabindex="-1" id="offCanvasMenu" aria-labelledby="offcanvasDarkLabel">
-    </section>
- )
-}
+// function Game() {
+//  return (
+//     // <!-- think I'm going to use an offcanvas component for the side nav -->
+//     <section classNameName="titleCon">
+//         <p classNameName="text-start fs-1">
+//             Game Title
+//         </p>
+//         <button className="btn" data-bs-toggle="offcanvas" href="#offCanvasMenu" role="button" aria-controls="offCanvasMenu" >
+//             menu
+//         </button>
+//     </section>
+//     // <!-- offcanvas -->
+//     <section className="offcanvas offcanvas-end show text-bg-dark" tabindex="-1" id="offCanvasMenu" aria-labelledby="offcanvasDarkLabel">
+//     </section>
+//  )
+// }
 
-export default Game;
+// export default Game;
+
+export default function Game() {
+    return (
+        // <!-- think I'm going to use an offcanvas component for the side nav -->
+        <section className="titleCon">
+            <p className="text-start fs-1">
+                Game Title
+            </p>
+            <button className="btn" data-bs-toggle="offcanvas" href="#offCanvasMenu" role="button" aria-controls="offCanvasMenu" >
+                menu
+            </button>
+        </section>
+        // <!-- offcanvas -->
+        <section className="offcanvas offcanvas-end show text-bg-dark" tabindex="-1" id="offCanvasMenu" aria-labelledby="offcanvasDarkLabel">
+        </section>
+     )
+}
