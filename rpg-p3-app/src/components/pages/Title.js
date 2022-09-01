aeb7b118889675d112b9dde85e2c2c83a9dad862
LindsayLew <lmschwehr@gmail.com>
2022-09-01 14:13:59 -0700

    fixing more bootstrap stuff

--- rpg-p3-app/src/components/pages/Title.js
+++ rpg-p3-app/src/components/pages/Title.js
@@ -1,6 +1,7 @@
 import './style/title.css'
 // import 'url("https://fonts.googleapis.com/css2?family=Silkscreen&display=swap")'
 
+import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 // export default function Title() {
@@ -23,9 +24,9 @@ function Title({ currentPage, handlePageChange}) {
         <section className="h-100" id="contentCon">
             <p className="text-wrap text-white fs-1" id="title">Game Title</p>
             <Link onClick={() => handlePageChange('/Home')}>
-                <button type="button" id="startButton" class="btn bg-secondary text-light p-3 w-25 align-self-center">
+                <Button type="button" id="startButton" class="btn bg-secondary text-light p-3 w-25 align-self-center">
                     embark on adventure
-                </button>
+                </Button>
             </Link>
         </section>
     </section>
