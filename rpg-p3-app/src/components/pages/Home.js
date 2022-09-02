a2ce0083b4e90d03302e668c5e48fbb2d2e661cc
LindsayLew <lmschwehr@gmail.com>
2022-09-02 09:58:33 -0700

    fixing styling on home

--- rpg-p3-app/src/components/pages/Home.js
+++ rpg-p3-app/src/components/pages/Home.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-    <section className="text-center vh-100">
+    <section id="pageCon">
         <section className="h-100" id="contentCon">
             <p className="text-wrap text-white fs-1 mb-5" id="title">Game Title</p>
             <Link to="/SignUp">
-                <Button type="button" id="newButton" className="btn text-light p-3 w-25 align-self-center mb-5">
+                <Button type="button" id="newButton" className="btn">
                     Create Account
                 </Button>
             </Link>
 
             <Link to="/Login">
-                <Button type="button" id="loginButton" className="btn text-light p-3 w-25 align-self-center mb-4">
+                <Button type="button" id="loginButton" className="btn">
                     Login
                 </Button>
             </Link>
