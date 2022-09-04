1cc9cff558f54a5f2b8ccbca5f5cd3b2fa9101f9
Hope Barrow <barr.writing@gmail.com>
2022-09-04 16:38:23 -0500

    set up base music file

--- rpg-p3-app/src/components/pages/Battlescreen/Music/index.js
+++ rpg-p3-app/src/components/pages/Battlescreen/Music/index.js
@@ -2,3 +2,58 @@ import { useState } from 'react';
 import './Media/Arabe-Africano.m4a';
 import './Media/Game-fight.m4a';
 import './Wii-Type-Tumbao.m4a';
+
+// Okay, so we have 3 songs that need to play
+// -- one for the fight screen
+// -- one for the VN
+// -- one for the title screen (???)
+
+// I'm going to write out this basic form, but I need all 3 songs to (a) loop, and (b) play for each section?
+
+// Game Fight
+const playFirst = (
+    handleSongLoading,
+    handleSongPlaying,
+    handleSongFinishedPlaying
+) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    //this is where it gets funky, I'd need it to play when battle starts/ends
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// VN section
+const playSecond = (
+    handleSongLoading,
+    handleSongPlaying,
+    handleSongFinishedPlaying
+) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    //this is where it gets funky, I'd need it to play when battle starts/ends
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+//Game Menu
+const playThird = (
+    handleSongLoading,
+    handleSongPlaying,
+    handleSongFinishedPlaying
+) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    //this is where it gets funky, I'd need it to play when battle starts/ends
+    return (
+        <div>
+            
+        </div>
+    )
+}
