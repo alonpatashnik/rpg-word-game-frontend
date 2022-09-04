78765cdf17f840e95a4c83ca24ab39565bba418b
LindsayLew <lmschwehr@gmail.com>
2022-09-04 13:44:30 -0700

    game window why

--- rpg-p3-app/src/components/pages/Battlescreen/BattlescreenPage/BS.js
+++ rpg-p3-app/src/components/pages/Battlescreen/BattlescreenPage/BS.js
@@ -1,6 +1,6 @@
 import './bs.css';
 import Form from 'react-bootstrap/Form';
-import Image from 'react-bootstrap/Image';
+// import Image from 'react-bootstrap/Image';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 // import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
@@ -24,13 +24,13 @@ export default function Battlescreen (){
     
     <section id = "playerIcon">
         <section className = "float-left">
-            <img className = "rounded" src = {require('../rpg-p3-app/imgs/PlaceholderPlayer1').default} height={200} width ={200}/>
+            <img className = "rounded" src = {require('../../../../imgs/PlaceholderPlayer1.png').default} height={200} width ={200}/>
                 <p>Player 1</p>
         </section>
     </section>,
 
         <section id = "enemyIcon">
-            <section className = "roundedCircle" src = {require('/imgs/PlaceholderPlayer2').default} height = {400} width = {400} />
+            <section className = "roundedCircle" src = {require('../../../../imgs/PlaceholderPlayer2.png').default} height = {400} width = {400} />
         </section>,
 
     <section id = "hpBar">
