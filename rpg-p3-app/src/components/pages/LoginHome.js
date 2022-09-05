3112e011d7dd56b722ffc2c0fe3b843d24ba3db8
alonpatashnik <alonp714@gmail.com>
2022-09-05 14:12:47 -0700

    minor tweak

--- rpg-p3-app/src/components/pages/LoginHome.js
+++ rpg-p3-app/src/components/pages/LoginHome.js
@@ -3,13 +3,13 @@ import './style/LoginHome.css'
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-export default function LoginHome({user}) {
+export default function LoginHome({user, logout}) {
     // fetch request here, authorization option with token on localstore
     return (
-    <section id="pageCon">
+    <section className="text-center vh-100">
         <section className="h-100" id="contentCon">
-            <p id="title">Welcome back, {user.username}</p>
-            <Link to='/game'>
+            <p className="text-wrap text-white fs-1 mb-5" id="welcome">Welcome back, {user.username}</p>
+            <Link to='/FauxGame'>
                 <Button type="button" id="newBtn" className="btn text-light p-3 w-25 align-self-center mb-5">
                     New Adventure
                 </Button>
@@ -17,6 +17,11 @@ export default function LoginHome({user}) {
             <Link to='/game'>
                 <Button type="button" id="continueBtn" className="btn text-light p-3 w-25 align-self-center mb-4">
                     Continue
+                </Button>
+            </Link>
+            <Link to='/Login'>
+                <Button type="button" id="logoutBtn" className="btn text-light p-3 w-25 align-self-center mb-4" onClick={logout}>
+                    Logout
                 </Button>
             </Link>
         </section>
