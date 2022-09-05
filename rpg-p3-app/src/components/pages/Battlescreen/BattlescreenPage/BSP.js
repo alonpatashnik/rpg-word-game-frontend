4890f9c17fc02731ecb7872e469fb94823997a2d
LindsayLew <lmschwehr@gmail.com>
2022-09-05 15:56:15 -0700

    modal for Aelin

--- rpg-p3-app/src/components/pages/Battlescreen/BattlescreenPage/BSP.js
+++ rpg-p3-app/src/components/pages/Battlescreen/BattlescreenPage/BSP.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../../style/bs.css'
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -15,9 +15,10 @@ function Battlescreen(){
         return (
             // <Router>
             <section className='bs'>
-                <AttackBar />
+                
                 <Avatar />
                 <Enemy />
+                <AttackBar />
                 <ProgressBar />
                 {/* <Timer /> */}
                 {/* <Routes> */}
