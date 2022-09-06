db3a90877913a35d0d8bc9f9e0c9a128df38faeb
Hope Barrow <barr.writing@gmail.com>
2022-09-06 18:34:54 -0500

    I am changing the character select page like a fool I have deleted everything to start anew

---
+++ rpg-p3-app/src/components/pages/characterSelect.js
@@ -0,0 +1,28 @@
+// state select -- state player select
+// change rendering on the page
+// clicking submit on page and taking information from state to use later on
+// using state for forms
+// passing information 
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bookstrap/Form';
+import React, { useState } from 'react';
+
+
+
+function characterSelect(){
+    return(
+        <h1>Welcome! Your journey begins now! Please select your hero.</h1>
+        
+)}
+
+
+
+
+
+
+
+// Battle
+
+// using information between selecting characters
+// if statements between players 1, 2, 3
