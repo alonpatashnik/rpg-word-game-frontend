e8e4c731f01087b4dffc312fac3c11d2f1a7bf67
LindsayLew <lmschwehr@gmail.com>
2022-09-07 11:58:49 -0700

    merging dev

--- rpg-p3-app/src/components/pages/Battlescreen/Avatar/index.js
+++ rpg-p3-app/src/components/pages/Battlescreen/Avatar/index.js
@@ -3,8 +3,8 @@
 export default function Avatars(){
     return (
         <section id='avatarCon'>
-            <img id="playerImage" alt='illustration of the headshot for the playable character' src = {require('../../../imgs/PlaceholderPlayer2.png')}/>
-            {/* <p>{user.username}</p> */}
+            <img id="playerImage" alt='illustration of the headshot for the playable character' src = {require('../../../imgs/Player1Fin.png')}/>
+            <p id="charName">Dray Woodflayer</p>
         </section>
     )
 };
