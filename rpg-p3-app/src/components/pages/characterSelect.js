f790872e883f0f1b36616e1eccdb64502463bed1
Hope Barrow <barr.writing@gmail.com>
2022-09-06 19:14:13 -0500

    added character select,no styling

--- rpg-p3-app/src/components/pages/characterSelect.js
+++ rpg-p3-app/src/components/pages/characterSelect.js
@@ -6,16 +6,66 @@
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bookstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import React, { useState } from 'react';
+import '../../src/components/imgs/Player1Fin.png';
+import '../../src/components/imgs/Player2Fin.png';
+import '../../src/components/imgs/Player3Fin.png';
 
 
 
-function characterSelect(){
-    return(
-        <h1>Welcome! Your journey begins now! Please select your hero.</h1>
-        
-)}
+function characterSelect() {
+    return (
+        <>
+            <section>
+                <h1>Welcome! Your journey begins now! Please select your hero.</h1>
+            </section>
 
+            <section id="Player 1">
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Player 1!</Modal.Title>
+                        <Modal.Body><img src={'../../src/components/imgs/Player1Fin.png'}></img></Modal.Body>
+                    </Modal.Header>
+                </Modal>
+            </section>
+
+            <section id="Player 2">
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Player 1!</Modal.Title>
+                        <Modal.Body><img src={'../../src/components/imgs/Player2Fin.png'}></img></Modal.Body>
+                    </Modal.Header>
+                </Modal>
+            </section>
+
+            <section id="Player 3">
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Player 1!</Modal.Title>
+                        <Modal.Body><img src={'../../src/components/imgs/Player3Fin.png'}></img></Modal.Body>
+                    </Modal.Header>
+                </Modal>
+            </section>
+
+            <section id="submitBtn">
+                <Button variant="primary" type="submit">Submit</Button>
+            </section>
+
+            <Form>
+                <Form.Group className="mb-3" controlId="characterName">
+                    <Form.Label>Please enter your name!</Form.Label>
+                    <Form.Control type="characterName" placeholder="Enter your name here." />
+                </Form.Group>
+
+                <Button variant="secondary" type="submit">Submit</Button>
+            </Form>
+
+        </>
+    )
+}
+
+export default characterSelect;
 
 
 
